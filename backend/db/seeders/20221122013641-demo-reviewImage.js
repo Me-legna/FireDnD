59a2a480df1e445cef7cb7deaea2e9f08544226c
Chino <111507103+Me-legna@users.noreply.github.com>
2022-11-21 21:21:07 -0500

    Add seed data to ReviewImages table

---
+++ backend/db/seeders/20221122013641-demo-reviewImage.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    options.tableName = 'ReviewImages';
+    await queryInterface.bulkInsert(options, [
+      {
+        reviewId:1,
+        url: 'img1spotId1.url'
+      },
+      {
+        reviewId:2,
+        url: 'img2spotId1.url'
+      },
+      {
+        reviewId:3,
+        url: 'img1spotId2.url'
+      },
+    ],{})
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'ReviewImages';
+    const Op = Sequelize.Op;
+    await queryInterface.bulkDelete(options,{
+      id: { [Op.in]: [1,2,3] }
+    }, {})
+  }
+};
