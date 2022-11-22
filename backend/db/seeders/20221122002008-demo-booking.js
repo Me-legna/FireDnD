77ca4851dee8e6b01dc809668242cdddd55711e1
Chino <111507103+Me-legna@users.noreply.github.com>
2022-11-21 19:46:15 -0500

    Add seed data to Bookings table

---
+++ backend/db/seeders/20221122002008-demo-booking.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    options.tableName = 'Bookings';
+    await queryInterface.bulkInsert(options, [
+      {
+        spotId: 1,
+        userId: 3,
+        startDate: '2024-11-24',
+        endDate: '2024-11-25',
+      },
+      {
+        spotId: 1,
+        userId: 3,
+        startDate: '2023-12-24',
+        endDate: '2023-12-25',
+      },
+      {
+        spotId: 2,
+        userId: 3,
+        startDate: '2025-10-24',
+        endDate: '2025-10-25',
+      },
+    ], {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'Bookings';
+    const Op = Sequelize.Op;
+    await queryInterface.bulkDelete(options, {
+      userId: 3
+    }, {})
+  }
+};
