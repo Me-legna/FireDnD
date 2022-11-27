34d0fb84ed1ec986dc8af1d304c16bb748f98253
Chino <111507103+Me-legna@users.noreply.github.com>
2022-11-27 04:29:12 -0500

    Add validateBookings error handling, Creat Booking by spotId, and Update Booking model

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -15,6 +15,11 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Booking.init({
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     spotId: {
       type: DataTypes.INTEGER,
       allowNull: false,
