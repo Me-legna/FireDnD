ea23ad89f4192b4d2fcfdb7b72603500ed57242e
Me-legna <111507103+Me-legna@users.noreply.github.com>
2022-12-09 20:07:39 -0500

    Add Profile Button

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,24 +1,43 @@
 // frontend/src/components/Navigation/ProfileButton.js
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
+import './Navigation.css'
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
 
   const logout = (e) => {
     e.preventDefault();
     dispatch(sessionActions.logout());
   };
 
-  const ulClassName = "profile-dropdown";
+  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = (e) => {
+      setShowMenu(false);
+    };
+
+    document.addEventListener('click', closeMenu);
+
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
 
   return (
     <>
-      <button>
+      <button onClick={openMenu}>
         <i className="fas fa-user-circle" />
       </button>
-      <ul className="profile-dropdown">
+      <ul className={ulClassName}>
         <li>{user.username}</li>
         <li>{user.firstName} {user.lastName}</li>
         <li>{user.email}</li>
