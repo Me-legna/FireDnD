fb2b0e7550ea2cc60690624adb54d643b167da5d
Me-legna <111507103+Me-legna@users.noreply.github.com>
2022-12-17 00:40:45 -0500

    Add ReviewsReducer and State

---
+++ frontend/src/store/reviews.js
@@ -0,0 +1,15 @@
+import { csrfFetch } from "./csrf";
+
+const initialState = {
+    spot: {},
+    user: {},
+}
+
+const reviewsReducer = (state = initialState, action) => {
+        switch (action.type) {
+            default:
+                return state
+        }
+    }
+
+export default reviewsReducer
