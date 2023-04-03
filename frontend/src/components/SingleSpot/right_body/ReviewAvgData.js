93a8f27da6406bee08ae7a5f83ecf62bb272cc47
Angel Gonzalez <111507103+Me-legna@users.noreply.github.com>
2023-04-02 19:16:23 -0500

    fix import typo

--- frontend/src/components/SingleSpot/right_body/ReviewAvgData.js
+++ frontend/src/components/SingleSpot/right_body/ReviewAvgData.js
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 import solidStar from "../../../images/spotImages/star-solid.svg";
-import logo from "../../images/fireDnD-logo.png";
+import logo from "../../../images/fireDnD-logo.png";
 
 
 function ReviewAvgData() {
